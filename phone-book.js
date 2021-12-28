224534c34a50febc0e13dfd9eacdb026851d6b95
Emmanuel Abaye <abayeemmanuel@yahoo.com>
2021-12-28 11:47:32 +0100

    implement simple phonebook

---
+++ phone-book.js
@@ -0,0 +1,50 @@
+class PhoneBook {
+  constructor() {
+    this.contacts = [];
+  }
+
+  addContact({ name, phoneNumber, email }) {
+    if(!phoneNumber) {
+      throw 'Contact phone number is invalid';
+    }
+
+    const contactAlreadyExists = this.getContact(phoneNumber)
+    
+    if(contactAlreadyExists) {
+        throw `Phone number ${phoneNumber} already exists in phone book`;
+    }
+
+    this.contacts.push({ name, phoneNumber, email, dateCreated: new Date() });
+  }
+
+  getAllContacts() {
+      return this.contacts;
+  }
+
+  getContact(phoneNumber){
+      return this.contacts.find(contact => contact.phoneNumber === phoneNumber)
+  }
+
+  removeContact(phoneNumber) {
+    this.contacts = this.contacts.filter(contact => contact.phoneNumber !== phoneNumber)
+  }
+}
+
+// Usage
+const simplePhoneBook = new PhoneBook();
+simplePhoneBook.addContact({
+  name: 'Emmanuel',
+  phoneNumber: '123456',
+  email: 'emmanuel@email.com'
+})
+
+simplePhoneBook.addContact({
+  name: 'Fred',
+  phoneNumber: '1234',
+  email: 'fred@email.com'
+})
+
+simplePhoneBook.removeContact('1234')
+
+console.log('Get all contacts ', simplePhoneBook.getAllContacts())
+console.log('Get a contact ', simplePhoneBook.getContact('1234'))
